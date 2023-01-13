f70075a351a8c508b1bdcf4cbccf4b6070f1e6c0
Torsten Long <torsten.s.long@gmail.com>
2023-01-13 22:01:58 +0100

    Refactor & make more functional

--- day01/app/Main.hs
+++ day01/app/Main.hs
@@ -4,39 +4,35 @@ import System.IO
 import System.IO (isEOF)
 import Data.List
 
-checkSum :: Integer
-checkSum = 2020
-
-tupleProd :: (Integer, Integer) -> Integer
-tupleProd (a,b) = a * b
-
-tupleSum :: (Integer, Integer) -> Integer
-tupleSum (a,b) = a + b
-
-hasSum :: Integer -> (Integer, Integer) -> Bool
-hasSum a b =  (tupleSum b) == a
-
-toPairs :: [a] -> [(a, a)]
-toPairs l = [ (x,y) | (x:xs) <- tails l, (y:ys) <- tails l ]
-
-toInt :: String -> Integer
-toInt s = read s :: Integer
-
 readStdin :: IO [String]
 readStdin = do
   contents <- readFile "/dev/stdin"
   return (words contents)
 
-pr (x, [l]) = (show x, show l)
+checkSum = 2020 :: Integer
 
-solvePart1 :: [String] -> Integer
-solvePart1 l = tupleProd $ head $ valid
+hasSum :: Integer -> [Integer] -> Bool
+hasSum a b =  (sum b) == a
+
+toPairs :: [a] -> [[a]]
+toPairs l = [ [x,y] | (x:xs) <- tails l, (y:ys) <- tails l ]
+
+toTriplets :: [a] -> [[a]]
+toTriplets l = [ [x,y,z] | (x:xs) <- tails l, (y:ys) <- tails l, (z:zs) <- tails l ]
+
+toInt :: String -> Integer
+toInt s = read s :: Integer
+
+solve :: ([Integer] -> [[Integer]]) -> [String] -> Integer
+solve conv l = foldl (*) 1 $ head $ valid
   where 
-    pairs = toPairs $ map toInt l
-    valid = filter (hasSum checkSum) pairs
+    combined = conv $ map toInt l
+    valid = filter (hasSum checkSum) combined
 
 main :: IO ()
 main = do
   lines <- readStdin
-  let out = solvePart1 lines
-  putStrLn (show out)
+  let part1 = solve toPairs lines
+  putStrLn (show part1)
+  let part2 = solve toTriplets lines
+  putStrLn (show part2)
