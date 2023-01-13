59f02e3dc5eb5dffebf41be174d1447aa70bfffd
Torsten Long <torsten.s.long@gmail.com>
2023-01-13 22:47:56 +0100

    Solve day 2 part 1

--- day02/app/Main.hs
+++ day02/app/Main.hs
@@ -7,20 +7,39 @@ import Data.List
 readStdin :: IO [String]
 readStdin = do
   contents <- readFile "/dev/stdin"
-  return (lines contents)
+  return $ lines contents
 
-toInt :: String -> Integer
-toInt str = read str :: Integer
+toInt :: String -> Int
+toInt str = read str :: Int
 
 splitOn :: (Char -> Bool) -> String -> [String]
-splitOn pred inStr =
-  case dropWhile pred inStr of
+splitOn pred inputStr =
+  case dropWhile pred inputStr of
     "" -> []
-    str -> w : splitOn pred broken
-      where (w, broken) = break pred str
+    str -> word : (splitOn pred rest)
+      where (word, rest) = break pred str
 
-solve :: [String] -> [[String]]
-solve l = map (splitOn (== ' ')) l
+parseRange :: String -> (Int, Int)
+parseRange rangeStr = (firstEl, lastEl)
+  where
+    firstEl = head els
+    lastEl = last els
+    els = map toInt $ splitOn (== '-') rangeStr
+
+parse :: [String] -> ((Int, Int), Char, String)
+parse l = ((parseRange range), char, passwd)
+  where
+    range = head l
+    char = head $ head $ drop 1 l
+    passwd = last l
+
+solution :: ((Int, Int), Char, String) -> Bool
+solution ((rStart, rEnd), c, s) = count >= rStart && count <= rEnd
+  where
+    count = length $ filter (== c) s
+
+solve :: [String] -> Int
+solve l = length $ filter solution $ map parse $ map (splitOn (== ' ')) l
 
 main :: IO ()
 main = do
