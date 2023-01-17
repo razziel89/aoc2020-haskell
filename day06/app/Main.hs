201c71ade324328053f50e8310569b5198bc1116
Torsten Long <torsten.s.long@gmail.com>
2023-01-17 19:12:44 +0100

    Make linter happy

--- day06/app/Main.hs
+++ day06/app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE
-  DeriveGeneric
+  DeriveGeneric,
+  TupleSections
 #-}
 module Main where
 
@@ -11,33 +12,31 @@ import GHC.Generics (Generic)
 import Data.Hashable
 
 readStdin :: IO String
-readStdin = do
-  contents <- readFile "/dev/stdin"
-  return contents
+readStdin = do readFile "/dev/stdin"
 
 splitOn :: (a -> Bool) -> [a] -> [[a]]
 splitOn pred input =
   case dropWhile pred input of
     [] -> []
-    l -> word : (splitOn pred rest)
+    l -> word : splitOn pred rest
       where (word, rest) = break pred l
 
 count :: (Eq a) => a -> [a] -> Int
 count val l = L.length $ L.filter (==val) l
 
 elemCounts :: (Ord a) => [[a]] -> Map a Int
-elemCounts ll = M.fromListWith (+) $ L.map (\v -> (v, 1)) $ concat ll
+elemCounts ll = M.fromListWith (+) $ L.map (, 1) $ concat ll
 
 main :: IO ()
 main = do
   contents <- readStdin
   -- Part 1.
   let groups = splitOn (=="") $ lines contents
-  let part1 = L.foldl (+) 0 $ L.map (\g -> S.size $ S.fromList $ concat g) groups
-  putStrLn (show part1)
+  let part1 = L.sum $ L.map (S.size . S.fromList . concat) groups
+  print part1
   -- Part 2.
   -- Keep only those values that have been specified as often as the list is
   -- long because the list has a length of the number of people in the list.
-  let listCounts = L.map (\g -> M.size $ M.filter (==(L.length g)) $ elemCounts g) groups
-  let part2 = L.foldl (+) 0 listCounts
-  putStrLn (show part2)
+  let listCounts = L.map (\g -> M.size $ M.filter (== L.length g) $ elemCounts g) groups
+  let part2 = L.sum listCounts
+  print part2
