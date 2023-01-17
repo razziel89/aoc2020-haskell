696e6c1e2e15593865a5b6a2c05fc6f5b95433e4
Torsten Long <torsten.s.long@gmail.com>
2023-01-17 19:03:17 +0100

    sSolve part 2 and refactoring

--- day06/app/Main.hs
+++ day06/app/Main.hs
@@ -23,21 +23,21 @@ splitOn pred input =
       where (word, rest) = break pred l
 
 count :: (Eq a) => a -> [a] -> Int
-count val (x:xs) = (if val == x then 1 else 0) + count val xs
+count val l = L.length $ L.filter (==val) l
 
--- listCounts :: [a] -> Map a Int
---
--- filterValues :: v -> Map k v -> Map k v
--- filterValues val map = M.filter (==val)
+elemCounts :: (Ord a) => [[a]] -> Map a Int
+elemCounts ll = M.fromListWith (+) $ L.map (\v -> (v, 1)) $ concat ll
 
 main :: IO ()
 main = do
   contents <- readStdin
   -- Part 1.
   let groups = splitOn (=="") $ lines contents
-  let part1 = L.foldl (+) 0 $ L.map S.size $ L.map S.fromList $ L.map concat groups
+  let part1 = L.foldl (+) 0 $ L.map (\g -> S.size $ S.fromList $ concat g) groups
   putStrLn (show part1)
-  -- -- Part 2.
-  -- -- Keep only those values that have been specified as often as the list
-  -- let realListCount = L.map (\g -> filterValues (L.length g) $ listCounts g) groups
-  -- let groupSets = L.fold (+) $ L.map (\s -> S.filter (\s -> S.size s) == )
+  -- Part 2.
+  -- Keep only those values that have been specified as often as the list is
+  -- long because the list has a length of the number of people in the list.
+  let listCounts = L.map (\g -> M.size $ M.filter (==(L.length g)) $ elemCounts g) groups
+  let part2 = L.foldl (+) 0 listCounts
+  putStrLn (show part2)
