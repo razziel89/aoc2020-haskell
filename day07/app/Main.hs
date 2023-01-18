02a8bbb1cc42df8c738513d0a7d1a363804d4046
Torsten Long <torsten.s.long@gmail.com>
2023-01-18 23:15:32 +0100

    Solve part 1

--- day07/app/Main.hs
+++ day07/app/Main.hs
@@ -43,7 +43,13 @@ newtype Bag =
 
 data Def =
   Def Bag (Map Bag Int)
-  deriving (Show)
+  deriving (Show, Ord, Eq)
+
+getBag :: Def -> Bag
+getBag (Def b m) = b
+
+getBags :: Def -> [Bag]
+getBags (Def b m) = M.keys m
 
 parse :: String -> Def
 parse l = Def bag map
@@ -62,9 +68,56 @@ parse l = Def bag map
            , toInt $ noToZero $ applyToWords (L.take 1) s))
         secondHalf
 
+shiny :: Bag
+shiny = Bag "shiny gold"
+
+emptyBag :: Bag
+emptyBag = Bag ""
+
+makeUnique :: (Ord a) => [a] -> [a]
+makeUnique = S.toList . S.fromList
+
+makeMap :: [Def] -> Map Bag [Bag]
+makeMap l = M.fromList $ L.map (\d -> (getBag d, getBags d)) l
+
+lookupWithPanic :: (Ord k) => Map k [v] -> k -> [v]
+lookupWithPanic m k =
+  if M.member k m
+    then M.findWithDefault [] k m
+    else error "cannot find bag"
+
+myTrace :: (Show a) => a -> a
+myTrace a = traceShow a a
+
+translate :: Map Bag [Bag] -> [Bag] -> [Bag]
+translate m l =
+  case L.length l of
+    0 -> l
+    1 ->
+      if L.head l == shiny
+        then l
+        else newlist
+    _ -> newlist
+  where
+    newlist = translate m $ makeUnique $ L.concatMap (lookupWithPanic m) l
+
+shinyRemains :: Map Bag [Bag] -> Map Bag [Bag]
+shinyRemains = M.mapWithKey f
+  where
+    f k v =
+      if k == shiny
+        then [shiny]
+        else v
+
 main :: IO ()
 main = do
   contents <- readStdin
   -- Part 1.
-  let groups = L.map parse $ lines contents
-  print groups
+  -- Filter out the shiny bag as key because we do not want to traverse that
+  -- bag. At least not for part 1.
+  let defs = shinyRemains $ makeMap $ L.map parse $ lines contents
+  print defs
+  let bags = L.filter (/= shiny) $ M.keys defs
+  let part1 =
+        L.length $ L.filter (== [shiny]) $ L.map (\b -> translate defs [b]) bags
+  print part1
