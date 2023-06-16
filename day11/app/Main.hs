e8581947f489ac7b94cbaf6f3d7ce97c494ad4f5
Torsten Long <torsten.s.long@gmail.com>
2023-06-16 23:06:07 +0200

    Add printing of intermediate state

--- day11/app/Main.hs
+++ day11/app/Main.hs
@@ -25,8 +25,18 @@ data Seat
   = EmptyS
   | OccupiedS
   | FloorS
+  -- This value will result in a panic, it is there because working with maybes
+  -- and error handling is a pain.
   | PanicS
   deriving (Show, Eq)
+
+printSeat :: Seat -> Char
+printSeat seat =
+  case seat of
+    EmptyS -> 'L'
+    FloorS -> '.'
+    OccupiedS -> '#'
+    _ -> error "cannot happen"
 
 charToSeat :: [Char] -> [Seat]
 charToSeat [] = []
@@ -43,7 +53,7 @@ listWithIndices :: [(Int, [(Int, a)])] -> [((Int, Int), a)]
 listWithIndices [] = []
 listWithIndices ((rowIdx, []):rows) = listWithIndices rows
 listWithIndices ((rowIdx, (colIdx, occ):cols):rows) =
-  ((rowIdx, colIdx), occ) : listWithIndices ((rowIdx, cols) : rows)
+  ((colIdx, rowIdx), occ) : listWithIndices ((rowIdx, cols) : rows)
 
 environment :: (Int, Int) -> [(Int, Int)]
 environment (x, y) =
@@ -71,11 +81,39 @@ countOcc map (x:xs) =
         OccupiedS -> True
         _ -> False
 
-occupiedNeighCount :: Map (Int, Int) Seat -> (Int, Int) -> Seat -> Int
+occupiedNeighCount :: Map (Int, Int) Seat -> (Int, Int) -> Seat -> (Seat, Int)
 occupiedNeighCount map coords seat =
   if seat == OccupiedS || seat == EmptyS
-    then countOcc map $ environment coords
-    else 0
+    then (seat, countOcc map $ environment coords)
+    else (seat, 0)
+
+nextState :: (Seat, Int) -> Seat
+nextState (seat, neighCount) =
+  case seat of
+    EmptyS ->
+      if neighCount == 0
+        then OccupiedS
+        else seat
+    OccupiedS ->
+      if neighCount >= 4
+        then EmptyS
+        else seat
+    PanicS -> error "cannot happen"
+    _ -> seat
+
+nextMapState :: Map (Int, Int) (Seat, Int) -> Map (Int, Int) Seat
+nextMapState = M.map nextState
+
+printMapWithSize :: (Int, Int) -> (Int, Int) -> Map (Int, Int) Seat -> String
+printMapWithSize coord@(x, y) max@(xmax, ymax) map = result
+  where
+    seat = M.findWithDefault PanicS coord map
+    result =
+      if x == xmax
+        then if y == ymax
+               then printSeat seat : "\n"
+               else printSeat seat : '\n' : printMapWithSize (0, y + 1) max map
+        else printSeat seat : printMapWithSize (x + 1, y) max map
 
 main :: IO ()
 main = do
@@ -84,5 +122,12 @@ main = do
   let mappable = listWithIndices rows
   let map = M.fromList mappable
   let counts = M.mapWithKey (occupiedNeighCount map) map
-  print map
-  print counts
+  -- print map
+  -- print counts
+  putStr $ printMapWithSize (0, 0) (9, 9) map
+  putStr "\n"
+  let nextMap = nextMapState counts
+  let nextCounts = M.mapWithKey (occupiedNeighCount nextMap) nextMap
+  -- print nextMap
+  putStr $ printMapWithSize (0, 0) (9, 9) nextMap
+  putStr "\n"
